27f394c122c2ee466d08d11fab04864bb4649cbb
Lefandoc <n.markelovv@gmail.com>
2022-05-18 23:57:11 +0300

    7. Spring Data

---
+++ src/main/java/ru/gb/lefandoc/springdatahw/data/Product.java
@@ -0,0 +1,35 @@
+package ru.gb.lefandoc.springdatahw.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String title;
+
+    private Integer price;
+
+    public Product(String title, Integer price) {
+        this.title = title;
+        this.price = price;
+    }
+}
